b431e2a92ada29217594d67bdb04a2a7343bbf98
Christian Kollmann <christian.kollmann@a-sit.at>
2024-04-25 21:31:04 +0200

    Rename "presentationType" to "representation"

--- service/src/main/kotlin/at/asit/apps/terminal_sp/prototype/server/QrCodeRequest.kt
+++ service/src/main/kotlin/at/asit/apps/terminal_sp/prototype/server/QrCodeRequest.kt
@@ -2,6 +2,6 @@ package at.asit.apps.terminal_sp.prototype.server
 
 data class QrCodeRequest(
     val credentialType: String,
-    val presentationType: String,
+    val representation: String,
     val attributes: List<String>,
 )
