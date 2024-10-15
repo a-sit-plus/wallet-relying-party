a88f5cf749b5fd6c39613d0ae986439c58da4a07
Christian Kollmann <christian.kollmann@a-sit.at>
2024-10-15 18:01:59 +0200

    Add expiration to transactions

--- service/src/main/kotlin/at/asit/apps/terminal_sp/prototype/server/TransactionStore.kt
+++ service/src/main/kotlin/at/asit/apps/terminal_sp/prototype/server/TransactionStore.kt
@@ -1,11 +1,15 @@
 package at.asit.apps.terminal_sp.prototype.server
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import okio.withLock
 import org.springframework.stereotype.Service
 import java.util.concurrent.locks.ReentrantLock
+import kotlin.time.Duration.Companion.minutes
 
 @Service
 class TransactionStore {
+    private val lifetime = 5.minutes
     private val lock = ReentrantLock()
     private val entries: MutableList<Entry> = mutableListOf()
 
@@ -14,6 +18,7 @@ class TransactionStore {
     fun getApiItem(id: String): ApiItem? = entries.firstOrNull { it.id == id }?.apiItem
 
     fun removeApiItem(id: String): ApiItem? = lock.withLock {
+        removeExpiredEntries()
         val entry = entries.firstOrNull { it.id == id }
         if (entry != null) {
             entries.remove(entry)
@@ -21,13 +26,17 @@ class TransactionStore {
         return entry?.apiItem
     }
 
-    fun put(id: String, user: Siop2User) {
-        lock.withLock {
-            user.toApiItem()?.let { entries.add(Entry(id, it)) }
-        }
+    fun put(id: String, user: Siop2User): Boolean? = lock.withLock {
+        removeExpiredEntries()
+        user.toApiItem()?.let { entries.add(Entry(id, it, Clock.System.now().plus(lifetime))) }
     }
+
+    private fun removeExpiredEntries() {
+        entries.removeAll { it.notAfter < Clock.System.now() }
+    }
+
 }
 
 
-data class Entry(val id: String, val apiItem: ApiItem)
+data class Entry(val id: String, val apiItem: ApiItem, val notAfter: Instant)
 
