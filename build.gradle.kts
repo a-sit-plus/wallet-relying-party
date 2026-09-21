99c750b4d11c41d0fc777c07225e13e3b29b6f83
gp-iaik <57531214+gp-iaik@users.noreply.github.com>
2026-09-21 13:04:37 +0200

    Build: Update Gradle tooling (#30)
    * chore: update Gradle tooling
    
    * Fix sync in IntelliJ 2026.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val kotlinVer = libs.versions.kotlin.get()
-    id("at.asitplus.gradle.conventions") version "20260701"
+    id("at.asitplus.gradle.conventions") version "20260828"
     kotlin("jvm") version kotlinVer apply false
     kotlin("plugin.serialization") version kotlinVer apply false
     kotlin("plugin.spring") version kotlinVer apply false
